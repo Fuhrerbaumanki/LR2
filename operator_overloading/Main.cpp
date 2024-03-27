6c6ee8eed4f0cc91e4f00d647493556b632e1e49
Fuhrer_baumanki <nicitow111@gmail.com>
2024-03-27 15:58:14 +0300

    Добавил тестирование классов

--- operator_overloading/Main.cpp
+++ operator_overloading/Main.cpp
@@ -2,23 +2,46 @@
 #include "Vector.h"
 #include <iostream>
 
-int main() {
-  Vector v1(3, 1.0);
-  Vector v2(3, 2.0);
-  Vector v3(3, 3.0);
-  double c1 = 2.0;
-  double c2 = 1.5;
-  double c3 = 0.5;
+int main(int argc, char *argv[]) {
+  const int vectorSize = 3;
+  double v1_data[vectorSize] = {1.0, 2.0, 3.0};
+  double v2_data[vectorSize] = {4.0, 5.0, 6.0};
+  double v3_data[vectorSize] = {7.0, 8.0, 9.0};
+  double c1_data[vectorSize] = {0.5, 0.5, 0.5};
+  double c2_data[vectorSize] = {1.0, 1.0, 1.0};
+  double c3_data[vectorSize] = {2.0, 2.0, 2.0};
 
-  CSLRMatrix m1(3);
-  m1(0, 0) = 1.0;
-  m1(1, 1) = 2.0;
-  m1(2, 2) = 3.0;
+  Vector v1(vectorSize);
+  Vector v2(vectorSize);
+  Vector v3(vectorSize);
+  Vector c1(vectorSize);
+  Vector c2(vectorSize);
+  Vector c3(vectorSize);
 
-  // Testing vector operations
-  Vector res = -c1 * m1 * (v1 - c2 * v2) + v3 * c3;
+  for (int i = 0; i < vectorSize; ++i) {
+    v1[i] = v1_data[i];
+    v2[i] = v2_data[i];
+    v3[i] = v3_data[i];
+    c1[i] = c1_data[i];
+    c2[i] = c2_data[i];
+    c3[i] = c3_data[i];
+  }
 
-  std::cout << "Result Vector: " << res << std::endl;
+  const int numRows = 3;
+  const int numNonZeros = 3;
+  double values[numNonZeros] = {1.0, 2.0, 3.0};
+  int columns[numNonZeros] = {0, 1, 2};
+  int rowPointers[numRows + 1] = {0, 1, 2, 3};
+
+  CSLRMatrix m1(numRows, numNonZeros, values, columns, rowPointers);
+
+  // Тестирование
+  Vector temp1 = v1 - (c2 * v2);
+  Vector temp2 = c1 * m1 * temp1;
+  Vector temp3 = temp2 + v3 * c3;
+  Vector res = -temp3;
+
+  std::cout << "Результат: " << res << std::endl;
 
   return 0;
 }
